0b975d07d53f3c7143c2f62622a6f4af3c27c63b
lsaperi <lsaperi>
2025-10-27 17:22:50 -0300

    gdgdfg; TaskID: dgfdgdfgd

---
+++ TABLEs/Creation-Scripts/new_taaccou tble.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `new_taaccou tble` (
+  `idnew_taaccou tble` int NOT NULL,
+  PRIMARY KEY (`idnew_taaccou tble`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='					'
